cb9732ee6139a45bbca27929cc4f94c9699510fa
Micah Geisel <micah@botandrose.com>
2023-03-18 09:09:01 +0100

    there's got to be a better way to do this.

--- lib/adva_blog.rb
+++ lib/adva_blog.rb
@@ -1,10 +1,14 @@
 # require "adva_blog/version"
 require "rails"
 
+# FIXME there's gotta be a better way!
+spec = Gem::Specification.find_by_name("adva")
+require "#{spec.gem_dir}/app/models/section"
+
 module AdvaBlog
   class Engine < Rails::Engine
     initializer "adva_blog.init" do
-      Section.register_type 'Blog'
+      ::Section.register_type 'Blog'
     end
   end
 end
