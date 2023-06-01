57e5efb8ef6d2a625d5e54ffd05db2f316a2fa41
Micah Geisel <micah@botandrose.com>
2023-06-01 17:19:41 +0200

    remove localization.

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -5,12 +5,12 @@ module BlogHelper
     month = archive_month(month) if month && !month.is_a?(Time)
 
     title = []
-    title << t(:'adva.blog.titles.date', :date => l(month, :format => '%B %Y')) if month
-    title << t(:'adva.blog.titles.about', :category => category.title) if category
-    title << t(:'adva.blog.titles.tags', :tags => tags.to_sentence) if tags
+    title << "from #{month.strftime('%B %Y')}" if month
+    title << "about #{category.title}" if category
+    title << "tagged #{tags.to_sentence}" if tags
     
     if title.present?
-      title = t(:'adva.blog.titles.articles', :articles => title.join(', ')) 
+      title = "Articles #{title.join(', ')}"
       options[:format] ? raw(options[:format]) % title : title
     end
   end
