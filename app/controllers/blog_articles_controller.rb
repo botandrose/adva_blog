fd5987b3517774ad1feebf408f013d99e95abd41
Micah Geisel <micah@botandrose.com>
2023-06-01 12:47:12 +0200

    try again to bring sanity to blog articles controller.

--- app/controllers/blog_articles_controller.rb
+++ app/controllers/blog_articles_controller.rb
@@ -1,20 +1,19 @@
 class BlogArticlesController < ArticlesController
   def index
-    if @section.categories.any?
-      @category ||= @section.categories.build(title: "All Categories", contents: @articles) 
+    articles
+
+    if defined?(Comment)
+      articles.includes!(:approved_comments_counter)
     end
-    if skip_caching? or stale?(:etag => @section, :last_modified => [@articles.to_a, @section, @site].flatten.collect(&:updated_at).compact.max.utc, :public => true)
+
+    # set after articles are fetched
+    @category ||= section.categories.build(title: "All Categories")
+
+    if skip_caching? or stale?([site, section], public: true)
       respond_to do |format|
         format.html { render :template => "blogs/articles/index" }
         format.atom { render :template => "blogs/articles/index", :layout => false }
       end
     end
-  end
-
-  public
-
-  def set_articles
-    super
-    @articles.includes!(:approved_comments_counter) if defined?(Comment)
   end
 end
